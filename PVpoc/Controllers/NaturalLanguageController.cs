ec9dfb19bf202a2e265e12cf31b844f88d94ee09
Ignatius Prinsloo <prinslooi@gmail.com>
2017-03-28 15:05:45 +0800

    Added swagger, updated the webApi controller and updated Newtonsoft to version 10.

--- PVpoc/Controllers/NaturalLanguageController.cs
+++ PVpoc/Controllers/NaturalLanguageController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using Google.Cloud.Language.V1;
-using static Google.Cloud.Language.V1.AnnotateTextRequest.Types;
+﻿using System.Web.Http;
 using PVpoc.GoogleAPI;
 
 namespace PVpoc.Controllers
 {
     public class NaturalLanguageController: ApiController
     {
+        //public string GetAnalyzeEntities(string text)
+        //{
+        //    return NaturalLanguage.AnalyzeEntitiesFromText(text).ToString();
+        //}
+
         public string GetAnnotatedText(string text)
         {
             return NaturalLanguage.AnalyzeEverything(text).ToString();
