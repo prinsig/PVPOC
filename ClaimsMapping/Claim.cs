7f14a4a22c3bc484e29842ddbcd04ad15e9f782c
Aaron Bridgeman <aaron.bridgeman@rac.com.au>
2017-03-28 13:50:56 +0800

    Add claims mapping project

---
+++ ClaimsMapping/Claim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimsMapping
+{
+    public class Claim
+    {
+        public enum DamageType
+        {
+            Lost,
+            Damaged,
+            Stolen
+        }
+
+        public string DamagedItem { get; set; }
+        public DamageType TypeOfDamage { get; set; }
+        public string DamageLocation { get; set; }
+        public string DateOfDamage { get; set; }
+        public string PolicyNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
