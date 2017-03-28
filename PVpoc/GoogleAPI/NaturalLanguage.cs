ac42a6e28155708cb8d16506061bae3c90357d28
Ignatius Prinsloo <prinslooi@gmail.com>
2017-03-28 11:58:58 +0800

    Add Google NLP and encrypted project info object.

---
+++ PVpoc/GoogleAPI/NaturalLanguage.cs
@@ -0,0 +1,18 @@
+﻿
+using Google.Cloud.Language.V1;
+
+namespace PVpoc.GoogleAPI
+{
+    public class NaturalLanguage
+    {
+        public void AnalyzeEntitiesFromText(string text)
+        {
+            var client = LanguageServiceClient.Create();
+            var response = client.AnalyzeEntities(new Document()
+            {
+                Content = text,
+                Type = Document.Types.Type.PlainText
+            });            
+        }
+    }
+}
