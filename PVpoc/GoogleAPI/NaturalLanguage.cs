89999fa06cea7b69492a2dc8b8660a79c98f984d
Ignatius Prinsloo <prinslooi@gmail.com>
2017-03-28 12:14:10 +0800

    Added annotateText

--- PVpoc/GoogleAPI/NaturalLanguage.cs
+++ PVpoc/GoogleAPI/NaturalLanguage.cs
@@ -1,18 +1,56 @@
-﻿
-using Google.Cloud.Language.V1;
+﻿using Google.Cloud.Language.V1;
+using static Google.Cloud.Language.V1.AnnotateTextRequest.Types;
 
 namespace PVpoc.GoogleAPI
 {
     public class NaturalLanguage
     {
-        public void AnalyzeEntitiesFromText(string text)
+        /// <summary>
+        /// language.documents.analyzeEntities
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public AnalyzeEntitiesResponse AnalyzeEntitiesFromText(string text)
+        {           
+            var client = LanguageServiceClient.Create();
+
+            if(client != null)
+            {
+                return client.AnalyzeEntities(new Document()
+                {
+                    Content = text,
+                    Type = Document.Types.Type.PlainText
+                });
+            }           
+
+            return null;           
+        }
+
+        /// <summary>
+        /// language.documents.annotateText
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public AnnotateTextResponse AnalyzeEverything(string text)
         {
             var client = LanguageServiceClient.Create();
-            var response = client.AnalyzeEntities(new Document()
+
+            if(client != null)
             {
-                Content = text,
-                Type = Document.Types.Type.PlainText
-            });            
+                return client.AnnotateText(new Document()
+                {
+                    Content = text,
+                    Type = Document.Types.Type.PlainText
+                },
+                new Features()
+                {
+                    ExtractSyntax = true,
+                    ExtractDocumentSentiment = true,
+                    ExtractEntities = true,
+                });
+            }
+
+            return null;                    
         }
     }
 }
