d7dd392549af6510b451b2ae1c614a3f94ce7a41
Aaron Bridgeman <aaron.bridgeman@rac.com.au>
2017-03-28 14:07:11 +0800

    First unit test for claims mapping

---
+++ ClaimsMapping.Tests/ClaimsMappingTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Google.Apis.CloudNaturalLanguage.v1.Data;
+using NUnit.Framework;
+
+namespace ClaimsMapping.Tests
+{
+    [TestFixture]
+    public class ClaimsMappingTests
+    {
+        [Test]
+        public void TestEntityMapping()
+        {
+            var atr = new AnnotateTextResponse
+            {
+                Entities = new List<Entity>()
+                {
+                    new Entity()
+                    {
+                        Name = "iPhone",
+                        Type = "CONSUMER_GOOD"
+                    },
+                    new Entity()
+                    {
+                        Name = "beach",
+                        Type = "LOCATION"
+                    }
+                },
+                Tokens = new List<Token>()
+            };
+            
+            var claim = new ClaimPopulator().PopulateClaim(atr);
+            Assert.AreEqual("iPhone", claim.DamagedItem);
+            Assert.AreEqual("beach", claim.DamageLocation);
+        }
+    }
+}
