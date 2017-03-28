cccf086ace682f4607a4acc6addc8cce9ca3bccc
Ignatius Prinsloo <prinslooi@gmail.com>
2017-03-28 14:42:21 +0800

    Added webApi controller for the anotated text.

---
+++ PVpoc/Controllers/NaturalLanguageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Google.Cloud.Language.V1;
+using static Google.Cloud.Language.V1.AnnotateTextRequest.Types;
+using PVpoc.GoogleAPI;
+
+namespace PVpoc.Controllers
+{
+    public class NaturalLanguageController: ApiController
+    {
+        public string GetAnnotatedText(string text)
+        {
+            return NaturalLanguage.AnalyzeEverything(text).ToString();
+        }
+    }
+}
