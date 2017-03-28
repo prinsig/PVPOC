54a6c4590cbe859cf06a101c48b3aacefcc577a1
Ignatius Prinsloo <prinslooi@gmail.com>
2017-03-28 15:27:44 +0800

    Updated the routing for the wep api.

--- PVpoc/Controllers/NaturalLanguageController.cs
+++ PVpoc/Controllers/NaturalLanguageController.cs
@@ -5,11 +5,13 @@ namespace PVpoc.Controllers
 {
     public class NaturalLanguageController: ApiController
     {
+        //[Route("AnalyzeEntities")]
         //public string GetAnalyzeEntities(string text)
         //{
         //    return NaturalLanguage.AnalyzeEntitiesFromText(text).ToString();
         //}
 
+        [Route("AnnotatedText")]
         public string GetAnnotatedText(string text)
         {
             return NaturalLanguage.AnalyzeEverything(text).ToString();
